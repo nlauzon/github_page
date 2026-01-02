4f249ec482bc78880ecedff9ea16e51a9e91375b
Nicolas Lauzon <n.lauzon@iwscorp.com>
2026-01-01 20:56:27 -0700

    Adding files

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TempConverter from './components/TempConverter.vue'
+</script>
+
+<template>
+  <TempConverter/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TempConverter.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const celcius = ref(0)
+const fahrenheit = ref(32)
+
+function onCelcius(e) {
+    celcius.value = e.target.value
+    fahrenheit.value = celcius.value * 1.8 + 32
+}
+
+function onFahrenheit(e) {
+    fahrenheit.value = e.target.value
+    celcius.value = (fahrenheit.value - 32) / 1.8
+}
+</script>
+
+<template>
+    <input type="number" :value="celcius" @change="onCelcius">
+    Celcius =
+    <input type="number" :value="fahrenheit" @change="onFahrenheit">
+    Fahrenheit
+</template>
